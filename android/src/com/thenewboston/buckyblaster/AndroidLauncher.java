2a0f1197680fc906b28cc6dcdf19b110ff522bbc
john <lrc272317227@gmail.com>
2016-11-09 21:35:51 -0800

    Initial commit

---
+++ android/src/com/thenewboston/buckyblaster/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.thenewboston.buckyblaster;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.thenewboston.buckyblaster.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
